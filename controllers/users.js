97406538dca54b2cc479f8ad9ade0c34aa33ffa5
A.Sofiane Guerfi <schguerfi@gmail.com>
2018-05-07 13:19:25 -0700

    db_err_handl

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,28 @@ const User = require('../models/user');
 
 module.exports = {
     signUp: async (req, res, next) => {
-        // email and password 
-       console.log('SignUp bein called')
+    // email and password 
        const{ email, password } = req.value.body;
+
+    // check if user exist
+      const foundUser = await User.findOne({ email })
+      if (foundUser) {
+          return res.status(403).send({ error: "Email is already in use" })
+      }
+
     // save it in the db
        const newUser = new User({ email, password })
        await newUser.save();
 
+       // respond with token
        res.json({ user: 'created' });
       
     },
     signIn: async (req, res, next) => {
         // generate token
-        console.log('Signin bein called')
+        
     },
     secret: async (req, res, next) => {
-        console.log('secret bein called')
+       
      }
 }
