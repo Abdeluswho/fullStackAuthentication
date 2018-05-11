3d1fb81fcaab51c88429e3bd7a27b531485f9c3e
A.Sofiane Guerfi <schguerfi@gmail.com>
2018-05-11 11:33:03 -0700

    adds-on

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 const Schema = mongoose.Schema;
 
 // Create Schema
@@ -12,6 +13,18 @@ const userSchema = new Schema ({
     password: {
         type: String,
         required: true
+    }
+})
+
+userSchema.pre('save', async function (next) {
+    try {
+        // Generate a salt
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(this.password, salt)
+        this.password = passwordHash;
+        next();
+    } catch (error) {
+        next(error)
     }
 })
 
